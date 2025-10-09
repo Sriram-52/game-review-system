807021c0da7d0e906210f902588806d83528bb14
Sriram-52 <sriram.nyshadham@ateliasoftware.com>
2025-10-08 22:51:49 -0400

    Add Apollo Server setup with GraphQL schema and dependencies

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "./schema";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const apolloServer = new ApolloServer({
+	typeDefs,
+});
+
+const { url } = await startStandaloneServer(apolloServer, {
+	listen: { port: 4000 },
+});
+
+console.log("Server started on port", url);
